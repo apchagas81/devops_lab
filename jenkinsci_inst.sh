aef1e8682e0be539551078a03e23a4afcaf0cbf8
Andre CHAGAS <apchagas81@gmail.com>
2019-08-02 13:05:14 -0300

    updated jenkinsci_inst.sh script - 2

--- jenkinsci_inst.sh
+++ jenkinsci_inst.sh
@@ -17,5 +17,5 @@ sudo docker build -t vm-lab-jks-$now $docker_dir
 echo "Running the first container after provision process..... "
 sudo docker run -d -i -t --name vm-lab-jks-001 --hostname vm-lab-jks-001 -v /opt/app:/opt/app -p 8080:8080 -p 80:80 vm-lab-jks-$now /bin/bash
 echo "To connect to the container, run the following as root: docker exec -ti vm-lab-jks-001 /bin/bash....   "
-echo "To run a new container, type the following command in example: sudo docker run -d -i -t --name vm-lab-jks-002 --hostname vm-lab-jks-002 -v $app_home:$app_home -p 8081:8080 -p 81:80 vm-lab-jks-$now /bin/bash....   "
+echo "To run a new container, type the following command in example: sudo docker run -d -i -t --name vm-lab-jks-002 --hostname vm-lab-jks-002 -v /opt/app:/opt/app -p 8081:8080 -p 81:80 vm-lab-jks-$now /bin/bash....   "
 echo "script finished at $now"
